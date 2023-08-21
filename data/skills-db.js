5aa813399902530c59c7418fdf777a501abe5d0d
Kateattardo <youremail@domain.com>
2023-08-20 21:32:51 -0400

    data/skills-db.js

--- data/skills-db.js
+++ data/skills-db.js
@@ -1,11 +1,32 @@
 const skills = [
-    {skill: 'watching lessons', done: true},
-    {skill: 'complete code-alongs', done: true},
-    {skill: 'complete SEI immersive', done: false}
+    {id: 1, skill: 'watching lessons', done: true},
+    {id: 2, skill: 'complete code-alongs', done: false},
+    {id: 3, skill: 'complete SEI immersive', done: false},
+    {id: 4, skill: 'HTML', done: true},
+    {id: 5, skill: 'CSS', done: true},
+    {id: 6, skill: 'javascript',done: true},
 ];
+function getAll() {
+  return skills;
+};
 
-module.exports = {
-    getAll: function() {
-      return skills;
-    }
+
+  module.exports = {
+    getAll,
+   // getOne,
+    create,
+    deleteOne
+};
+	
+function deleteOne(id) {
+  id = parseInt(id);
+  const idx = skills.findIndex(skill => skill.id === id);
+  skills.splice(idx, 1);
+};
+      
+function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.level = "beginner";
+    skills.push(skill);
   };
+	
