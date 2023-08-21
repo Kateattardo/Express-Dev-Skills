63d282b24ed2254daa19b68455255f39fc91999a
Kateattardo <youremail@domain.com>
2023-08-20 21:08:41 -0400

    express-skills/models/skills.js

--- express-skills/models/skills.js
+++ express-skills/models/skills.js
@@ -1,21 +1,41 @@
 const skills = [
-    {id: 125223, skill: 'watching lessons', done: true},
-    {id: 127904, skill: 'complete code-alongs', done: true,
-    {id: 139608, todo: 'complete SEI', done: false}
+    {id: 1, skill: "watching lessons", level: advanced},
+    {id: 2, skill: "complete code-alongs", level: advanced},
+    {id: 3, skill: "complete SEI courses", level: basic},
+    {id: 4, skill: "javascript", level: basic},
+    {id: 5, skill: "html", level: intermediate},
+    {id  6, skill: "CSS", level: intermediate},
+    {id  7, skill: "node.js" level: basic},
+    {id  8, skill: "express" level: basic},
+
   ];
-	
-  module.exports = {
-    getAll
-    getOne
-  };
-	
   function getAll() {
     return skills;
   }
-  function getOne(id) {
-    // URL params are strings - convert to a number
+	
+  module.exports = {
+    getAll
+    //getOne,
+    create,
+    deleteOne
+  };
+  function deleteOne(id) {
     id = parseInt(id);
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+  };
+        
+  function create(skill) {
+      skill.id = Date.now() % 1000000;
+      skill.level = "beginner";
+      skills.push(skill);
+    };
+	
+
+  //function getOne(id) {
+    // URL params are strings - convert to a number
+   // id = parseInt(id);
     // The Array.prototype.find iterator method is
     // ideal for finding objects within an array
-    return skills.find(skill => skill.id === id);
-  }
+    //return skills.find(skill => skill.id === id);
+  
