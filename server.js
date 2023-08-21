5d481244d9b1f1b0cc46ee64edbce9861ec8c1db
Kateattardo <youremail@domain.com>
2023-08-20 21:00:57 -0400

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 // Require modules
 const express = require('express');
+const path = require('path');
+
+// require the skills "database"
+const todoDb = require('./data/skills-db');
   
 // Create the Express app
 const app = express();
@@ -12,8 +16,18 @@ const app = express();
   
 // Mount routes
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World!</h1>');
+  res.redirect('home');
 });
+
+app.get('/skills', function(req, res) {
+    res.render('skills/index', {
+      todos: todoDb.getAll()
+    });
+  });
+
+// Configure the app (app.set)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
   
 // Tell the app to listen on port 3000
 app.listen(3000, function() {
